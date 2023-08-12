8e23221a4a8776e5c604670a69b27b0bf505e5b5
navidAbbasian <abbasian.navid@gmail.com>
2023-08-12 13:10:46 +0330

    login and register done but need refresh token and refresh password

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\RegisterUserRequest;
 use App\Models\User;
+use GuzzleHttp\Client;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -48,6 +48,18 @@ class AuthController extends Controller
 
                 $data['token'] = $user->createToken('MyApp')->accessToken;
                 $data['name'] = $user->name;
+
+//                $http = new Client();
+//                $response = $http->request('POST', '127.0.0.1:8000/oauth/token', [
+//                    'form_params' => [
+//                        'grant_type' => 'password',
+//                        'client_id' => 2,
+//                        'client_secret' => 'Z1xaAaKwg0LIQiVDPW9DRKrdmlzEycEXzxeOhaC1',
+//                        'username' => 'navid@navid.com',
+//                        'password' => 12345678,
+//                        'scope' => '*',
+//                    ],
+//                ]);
 
                 $response = [
                     'data' => $data,
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\RolePermissions\PermissionManagerController;
+use App\Http\Controllers\RolePermissions\RoleManagerController;
+use App\Http\Controllers\Shop\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+Route::post('permissions/attach', [PermissionManagerController::class, 'attach']);
+
+Route::post('roles/assign', [RoleManagerController::class, 'assign']);
+Route::post('roles', [RoleManagerController::class, 'create']);
+Route::get('roles', [RoleManagerController::class, 'getAll']);
+
+Route::post('products', [ProductController::class, 'create'])->middleware('auth');
