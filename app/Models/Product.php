6d56c1b20e16607cf8e2617eaef4f848cf9d1667
navidAbbasian <abbasian.navid@gmail.com>
2023-08-12 20:23:21 +0330

    Uuid

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Http\Traits\UsesUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, UsesUuid;
 
     protected $guarded =[];
+
 }
