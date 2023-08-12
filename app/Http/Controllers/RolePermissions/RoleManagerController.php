773785e65fa2eff6fbde88468b2d1a80087a4320
navidAbbasian <abbasian.navid@gmail.com>
2023-08-12 15:59:11 +0330

    assign role to user

--- app/Http/Controllers/RolePermissions/RoleManagerController.php
+++ app/Http/Controllers/RolePermissions/RoleManagerController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\RolePermissions;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateRoleRequest;
+use http\Client\Curl\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -21,5 +23,13 @@ class RoleManagerController extends Controller
     {
         $role = Role::all();
         return response()->json($role, Response::HTTP_OK);
+    }
+
+    public function assign(Request $request)
+    {
+        $user = \App\Models\User::find($request['user_id']);
+        $user->assignRole($request['role_ids']);
+
+        return response()->json($user, Response::HTTP_OK);
     }
 }
